f890bf636c43c2ccd789fab0f82aa9bc05f76085
Jimmy Lam <dev.jimmylam@gmail.com>
2020-07-23 15:21:12 -0700

    created TODOs

--- App.js
+++ App.js
@@ -1,15 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Scratch and Win Game</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Dimensions
+} from "react-native";
+import { Button } from "native-base";
+import { FontAwesome } from "@expo/vector-icons";
+
+//TODO: get array of length 25
+var itemArray = new Array(25).fill('empty');
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      randomNumber: "",
+    };
+  }
+
+  //when app loads up
+  componentDidMount() {
+    //TODO: call generate randome number method
+  }
+
+  generateRandomNumber = () => {
+    //TODO: generate randome number
+  };
+
+  scratchItem = () => {
+    //TODO: decide lucky or unlucky
+  }
+
+  scratchItemIcon = () => {
+    //TODO: find right icon
+  }
+
+  scratchItemColo = () => {
+    //TODO: find right color
+  }
+
+  showAllItem = () => {
+    //TODO: Button - reveal all icons
+  }
+
+  resetGame = () => {
+    //TODO: Button - reset game
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Scratch and Win Game!</Text>
+      </View>
+    );
+  }
 }
+
 
 const styles = StyleSheet.create({
   container: {
