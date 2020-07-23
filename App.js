4137b29e4a5945cb15d052b9ad27bb66fcbea9e9
Jimmy Lam <dev.jimmylam@gmail.com>
2020-07-23 15:38:01 -0700

    added methodes for resetGame

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 
 import React from "react";
+import { Button } from "native-base";
+import { FontAwesome } from "@expo/vector-icons";
 import {
   StyleSheet,
   Text,
@@ -7,8 +9,7 @@ import {
   TouchableOpacity,
   Dimensions
 } from "react-native";
-import { Button } from "native-base";
-import { FontAwesome } from "@expo/vector-icons";
+
 
 //TODO: get array of length 25
 var itemArray = new Array(25).fill('empty');
@@ -24,31 +25,68 @@ export default class App extends React.Component {
   //when app loads up
   componentDidMount() {
     //TODO: call generate randome number method
+    this.setState({
+      randomNumber: this.generateRandomNumber()
+    });
   }
 
   generateRandomNumber = () => {
     //TODO: generate randome number
+    let randomNumber = Math.floor(Math.random() * 25)
+    this.setState({
+      randomNumber: randomNumber,
+      isScratched: true
+    });
+    return randomNumber;
   };
 
-  scratchItem = () => {
+  scratchItem = itemNumber => {
     //TODO: decide lucky or unlucky
-  }
+    if (this.state.randomNumber === itemNumber) {
+      itemArray[itemNumber] = "lucky"
+    } else {
+      itemArray[itemNumber] = "unlucky"
+    }
+    //update the state
+    this.forceUpdate();
+  };
 
   scratchItemIcon = () => {
     //TODO: find right icon
-  }
+    if (itemArray[itemNumber] === "lucky") {
+      return "dollar"
+    } else if (itemArray[itemNumber] === "unlucky") {
+      return "frown-o"
+    }
+    return "circle"
+  };
 
-  scratchItemColo = () => {
+  scratchItemColor = () => {
     //TODO: find right color
-  }
+    if (itemArray[itemNumber] === "lucky") {
+      return "green"
+    } else if (itemArray[itemNumber] === "unlucky") {
+      return "red"
+    }
+    return "black"
+  };
 
   showAllItem = () => {
     //TODO: Button - reveal all icons
+    itemArray.fill('unlucky');
+    itemArray[this.state.randomNumber] = 'lucky'
+
+    this.forceUpdate();
   }
 
   resetGame = () => {
     //TODO: Button - reset game
-  }
+    this.setState({ randomNumber: this.generateRandomNumber() },
+      () => {
+        itemArray.fill('empty');
+        this.forceUpdate;
+      });
+  };
 
   render() {
     return (
